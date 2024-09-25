c8096c1b216d25888c1bb51871ae71d01140db6e
RepositoryAnonymous <AnonymousRepository@outlook.com>
2024-09-25 16:09:55 +0000

    Add some comments

--- llvm/examples/Kaleidoscope/Chapter2/toy.cpp
+++ llvm/examples/Kaleidoscope/Chapter2/toy.cpp
@@ -13,19 +13,28 @@
 
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
+// [0-255] means the ascii value of an unknown character like '+'.
 enum Token {
-  tok_eof = -1,
+  tok_eof = -1,        // EOF: In C language, you can use functions such as fgetc, 
+                       // fgets, or fread to read files, and use the return value 
+                       // to determine whether EOF has been reached.
 
   // commands
-  tok_def = -2,
-  tok_extern = -3,
+  tok_def = -2,        // The def command.
+  tok_extern = -3,     // The extern command.
 
   // primary
-  tok_identifier = -4,
-  tok_number = -5
+  tok_identifier = -4, // The identifier identified by the Lexer is usually used 
+                       // to naming the symbols of the name variables, functions, 
+                       // classes or other entities.
+  tok_number = -5      // The numeric value.
 };
 
+// If the current token is an identifier, the `IdentifierStr` global variable ho-
+// lds the name of the identifier.
 static std::string IdentifierStr; // Filled in if tok_identifier
+// If the current token is a numeric literal (like 1.0), the `NumVal` global vari-
+// able holds its value.
 static double NumVal;             // Filled in if tok_number
 
 /// gettok - Return the next token from standard input.
@@ -36,18 +45,27 @@ static int gettok() {
   while (isspace(LastChar))
     LastChar = getchar();
 
+  // `isalpha()` is a standard library function that checks if a character is a 
+  // letter.
   if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
     IdentifierStr = LastChar;
+
+    // `isalnum()` is a standard library function that checks whether a character 
+    // is a letter or a number. If the current IdentifierStr starts from a letter
+    // and is followed by letters or numbers, then this is a complete Identifier.
     while (isalnum((LastChar = getchar())))
       IdentifierStr += LastChar;
 
-    if (IdentifierStr == "def")
+    if (IdentifierStr == "def")     // def
       return tok_def;
-    if (IdentifierStr == "extern")
+    if (IdentifierStr == "extern")  // extern
       return tok_extern;
-    return tok_identifier;
+    return tok_identifier;          // The symbols of the name variables, functions
+                                    // classes or other entities.
   }
 
+  // `isdigit()` is a standard library function that checks whether a character is 
+  // a digit character (i.e., the characters '0' to '9').
   if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
     std::string NumStr;
     do {
@@ -55,11 +73,13 @@ static int gettok() {
       LastChar = getchar();
     } while (isdigit(LastChar) || LastChar == '.');
 
+    // `strtod()` is a standard library function that converts a string to a double-
+    // precision floating point number.
     NumVal = strtod(NumStr.c_str(), nullptr);
     return tok_number;
   }
 
-  if (LastChar == '#') {
+  if (LastChar == '#') { // // Skip any comments lines.
     // Comment until end of line.
     do
       LastChar = getchar();
@@ -93,6 +113,9 @@ public:
 
 /// NumberExprAST - Expression class for numeric literals like "1.0".
 class NumberExprAST : public ExprAST {
+  // `NumberExprAST` class captures the numeric value of the literal as an instance 
+  // variable. This allows later phases of the compiler to know what the stored nu-
+  // meric value is.
   double Val;
 
 public:
@@ -104,12 +127,14 @@ class VariableExprAST : public ExprAST {
   std::string Name;
 
 public:
+  // A virtual method to pretty print the code.
   VariableExprAST(const std::string &Name) : Name(Name) {}
 };
 
 /// BinaryExprAST - Expression class for a binary operator.
 class BinaryExprAST : public ExprAST {
   char Op;
+  // `LHS` and `RHS` point to each `ExprAST` object.
   std::unique_ptr<ExprAST> LHS, RHS;
 
 public:
@@ -121,9 +146,11 @@ public:
 /// CallExprAST - Expression class for function calls.
 class CallExprAST : public ExprAST {
   std::string Callee;
+  // `Args` points to a list of `ExprAST` object.
   std::vector<std::unique_ptr<ExprAST>> Args;
 
 public:
+  // `calls` capture a function name as well as a list of any argument expressions.
   CallExprAST(const std::string &Callee,
               std::vector<std::unique_ptr<ExprAST>> Args)
       : Callee(Callee), Args(std::move(Args)) {}
@@ -163,7 +190,7 @@ public:
 /// CurTok/getNextToken - Provide a simple token buffer.  CurTok is the current
 /// token the parser is looking at.  getNextToken reads another token from the
 /// lexer and updates CurTok with its results.
-static int CurTok;
+static int CurTok;  // Point to an item of `enum Token`.
 static int getNextToken() { return CurTok = gettok(); }
 
 /// BinopPrecedence - This holds the precedence for each binary operator that is
@@ -172,6 +199,8 @@ static std::map<char, int> BinopPrecedence;
 
 /// GetTokPrecedence - Get the precedence of the pending binary operator token.
 static int GetTokPrecedence() {
+  // `isaSCII()` is a standard library function used to check whether the given 
+  // character is an ASCII character.
   if (!isascii(CurTok))
     return -1;
 
@@ -196,14 +225,22 @@ static std::unique_ptr<ExprAST> ParseExpression();
 
 /// numberexpr ::= number
 static std::unique_ptr<ExprAST> ParseNumberExpr() {
+  // This takes the current number value, creates a NumberExprAST node.
   auto Result = std::make_unique<NumberExprAST>(NumVal);
+  // This advances the lexer to the next token.
   getNextToken(); // consume the number
+  // Returns the NumberExprAST node.
   return std::move(Result);
 }
 
 /// parenexpr ::= '(' expression ')'
 static std::unique_ptr<ExprAST> ParseParenExpr() {
   getNextToken(); // eat (.
+  // Use recursion by calling `ParseExpression()`. It allows us to handle recur-
+  // sive grammars, and keeps each production very simple. Note that parentheses 
+  // do not cause construction of AST nodes themselves. While we could do it this 
+  // way, the most important role of parentheses are to guide the parser and pro-
+  // vide grouping. Once the parser constructs the AST, parentheses are not needed.
   auto V = ParseExpression();
   if (!V)
     return nullptr;
@@ -255,6 +292,10 @@ static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
 ///   ::= numberexpr
 ///   ::= parenexpr
 static std::unique_ptr<ExprAST> ParsePrimary() {
+  // Define a helper function to wrap all the simple expression-parsing logic to-
+  // gether into one entry point. Call this class of expressions "primary" expre-
+  // ssions. In order to parse an arbitrary primary expression, we need to deter-
+  // mine what sort of expression it is.
   switch (CurTok) {
   default:
     return LogError("unknown token when expecting an expression");
@@ -271,12 +312,56 @@ static std::unique_ptr<ExprAST> ParsePrimary() {
 ///   ::= ('+' primary)*
 static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
                                               std::unique_ptr<ExprAST> LHS) {
+  // The basic idea of operator precedence parsing is to break down an expression 
+  // with potentially ambiguous binary operators into pieces. Consider, for exam-
+  // ple, the expression "a+b+(c+d)*e*f+g". Operator precedence parsing considers 
+  // this as a stream of primary expressions separated by binary operators. As su-
+  // ch, it will first parse the leading primary expression "a", then it will see 
+  // the pairs [+, b] [+, (c+d)] [*, e] [*, f] and [+, g]. 
+  
+  // Note that because parentheses are primary expressions, the binary expression 
+  // parser doesn’t need to worry about nested subexpressions like (c+d) at all.
+  
+  // To start, an expression is a primary expression potentially followed by a se-
+  // quence of [binop,primaryexpr] pairs.
+  
+  // `ParseBinOpRHS()` is the function that parses the sequence of pairs. It takes 
+  // a precedence and a pointer to an expression for the part that has been parsed 
+  // so far. Note that "x" is a perfectly valid expression: As such, "binoprhs" is 
+  // allowed to be empty, in which case it returns the expression that is passed 
+  // into it.
+
+  // Like the expression "a+b+(c+d)*e*f+g", Primary == "a" and BinOpRHS is the pa-
+  // irs [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  // Like the expression "1+b+(c+d)*e*f+g", Primary == 1 and BinOpRHS is the pairs 
+  // [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  // Like the expression "(c+d*5)+b+(c+d)*e*f+g", Primary == "(c+d*5)" and BinOpRHS 
+  // is the pairs [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  
+  
+  /*
+            +
+          /   \
+         +     g
+       /   \
+      +     * 
+     / \   / \
+    a   b *   f
+         / \
+        e   +
+           / \
+          c   d
+  */
+
   // If this is a binop, find its precedence.
   while (true) {
     int TokPrec = GetTokPrecedence();
 
     // If this is a binop that binds at least as tightly as the current binop,
     // consume it, otherwise we are done.
+    // The priority of the currently obtained operator is not close enough to that 
+    // of the left-hand expression, so the parsed left-hand expression LHS is re-
+    // turned and parsing ends.
     if (TokPrec < ExprPrec)
       return LHS;
 
@@ -292,7 +377,13 @@ static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
     // If BinOp binds less tightly with RHS than the operator after RHS, let
     // the pending operator take RHS as its LHS.
     int NextPrec = GetTokPrecedence();
+    // If the priority of the current operator (TokPrec) is lower than NextPrec, 
+    // it means that the following operator needs to be combined with the current 
+    // RHS. By recursively calling ParseBinOpRHS, RHS is continued to be parsed 
+    // as the left subexpression, and the priority is increased (TokPrec + 1).
     if (TokPrec < NextPrec) {
+      // Like "1+1+1", passing TokPrec + 1 means we want to the first "1+1" to be 
+      // combined together.
       RHS = ParseBinOpRHS(TokPrec + 1, std::move(RHS));
       if (!RHS)
         return nullptr;
@@ -308,6 +399,14 @@ static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
 ///   ::= primary binoprhs
 ///
 static std::unique_ptr<ExprAST> ParseExpression() {
+  // Like the expression "a+b+(c+d)*e*f+g", Primary == "a" and BinOpRHS is the pa-
+  // irs [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  // Like the expression "1+b+(c+d)*e*f+g", Primary == 1 and BinOpRHS is the pairs 
+  // [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  // Like the expression "(c+d*5)+b+(c+d)*e*f+g", Primary == "(c+d*5)" and BinOpRHS 
+  // is the pairs [+, b] [+, (c+d)] [*, e] [*, f] and [+, g].
+  // So, `ParsePrimary()` needs to parse the expressions whose first token is iden-
+  // tifier, number, or '('.
   auto LHS = ParsePrimary();
   if (!LHS)
     return nullptr;
--- llvm/examples/Kaleidoscope/Chapter3/toy.cpp
+++ llvm/examples/Kaleidoscope/Chapter3/toy.cpp
@@ -103,7 +103,12 @@ class ExprAST {
 public:
   virtual ~ExprAST() = default;
 
+  // The `codegen()` method says to emit IR for that AST node along with all the 
+  // things it depends on, and they all return an LLVM Value object. "Value" is 
+  // the class used to represent a "Static Single Assignment (SSA) register" or 
+  // "SSA value" in LLVM.
   virtual Value *codegen() = 0;
+  // "= 0" means that it is a pure virtual function, it cannot be instantiated.
 };
 
 /// NumberExprAST - Expression class for numeric literals like "1.0".
@@ -399,9 +404,24 @@ static std::unique_ptr<PrototypeAST> ParseExtern() {
 // Code Generation
 //===----------------------------------------------------------------------===//
 
+// `TheContext` is an opaque object that owns a lot of core LLVM data structures, 
+// such as the type and constant value tables. We don't need to understand it in 
+// detail, we just need a single instance to pass into APIs that require it.
 static std::unique_ptr<LLVMContext> TheContext;
+// `TheModule` is an LLVM construct that contains functions and global variables. 
+// In many ways, it is the top-level structure that the LLVM IR uses to contain 
+// code. It will own the memory for all of the IR that we generate, which is why 
+// the codegen() method returns a raw Value*, rather than a unique_ptr<Value>.
 static std::unique_ptr<Module> TheModule;
+// The `Builder` object is a helper object that makes it easy to generate LLVM in-
+// structions. Instances of the `IRBuilder` class template keep track of the curr-
+// ent place to insert instructions and has methods to create new instructions.
 static std::unique_ptr<IRBuilder<>> Builder;
+// The `NamedValues` map keeps track of which values are defined in the current 
+// scope and what their LLVM representation is. (In other words, it is a symbol 
+// table for the code). In this form of Kaleidoscope, the only things that can be 
+// referenced are function parameters. As such, function parameters will be in 
+// this map when generating code for their function body.
 static std::map<std::string, Value *> NamedValues;
 
 Value *LogErrorV(const char *Str) {
@@ -409,12 +429,24 @@ Value *LogErrorV(const char *Str) {
   return nullptr;
 }
 
+// 将一个浮点数值（Val）转换为 LLVM IR 中的浮点常量表示，返回一个指向该浮点常量的指针（
+// Value*），以便在后续的代码生成过程中使用。Value* 是返回类型，表示返回一个指向 Value 
+// 对象的指针。Value 是 LLVM 中所有值的基类。
 Value *NumberExprAST::codegen() {
+  // ConstantFP 是 LLVM 中的一个类，用于表示浮点常量。get 是 ConstantFP 类的一个静态方
+  // 法，用于创建一个新的浮点常量。*TheContext 是一个指向 LLVM 上下文的指针。LLVMContext 
+  // 是 LLVM 中保存所有全局数据的容器。APFloat(Val) 是一个 APFloat 对象的构造函数调用，
+  // APFloat 是 LLVM 中表示任意精度浮点数的类。
   return ConstantFP::get(*TheContext, APFloat(Val));
 }
 
 Value *VariableExprAST::codegen() {
   // Look this variable up in the function.
+  // Assume that the variable has already been emitted somewhere and its value 
+  // is available. In practice, the only values that can be in the NamedValues 
+  // map are function arguments. This code simply checks to see that the speci-
+  // fied name is in the map (if not, an unknown variable is being referenced) 
+  // and returns the value for it.
   Value *V = NamedValues[Name];
   if (!V)
     return LogErrorV("Unknown variable name");
@@ -422,6 +454,10 @@ Value *VariableExprAST::codegen() {
 }
 
 Value *BinaryExprAST::codegen() {
+  // We recursively emit code for the left-hand side of the expression, then the 
+  // right-hand side, then we compute the result of the binary expression. In th-
+  // is code, we do a simple switch on the opcode to create the right LLVM ins-
+  // truction.
   Value *L = LHS->codegen();
   Value *R = RHS->codegen();
   if (!L || !R)
@@ -429,6 +465,14 @@ Value *BinaryExprAST::codegen() {
 
   switch (Op) {
   case '+':
+    // `IRBuilder` knows where to insert the newly created instruction, all you 
+    // have to do is specify what instruction to create (e.g. with CreateFAdd), 
+    // which operands to use (L and R here) and optionally provide a name for 
+    // the generated instruction.
+    // If the code above emits multiple "addtmp" variables, LLVM will automati-
+    // cally provide each one with an increasing, unique numeric suffix. Local 
+    // value names for instructions are purely optional, but it makes it much 
+    // easier to read the IR dumps.
     return Builder->CreateFAdd(L, R, "addtmp");
   case '-':
     return Builder->CreateFSub(L, R, "subtmp");
@@ -437,6 +481,10 @@ Value *BinaryExprAST::codegen() {
   case '<':
     L = Builder->CreateFCmpULT(L, R, "cmptmp");
     // Convert bool 0/1 to double 0.0 or 1.0
+    // `uitofp` instruction converts its input integer into a floating point 
+    // value by treating the input as an unsigned value. If we used the sitofp 
+    // instruction, the '<' operator would return 0.0 and -1.0, depending on 
+    // the input value.
     return Builder->CreateUIToFP(L, Type::getDoubleTy(*TheContext), "booltmp");
   default:
     return LogErrorV("invalid binary operator");
@@ -445,6 +493,11 @@ Value *BinaryExprAST::codegen() {
 
 Value *CallExprAST::codegen() {
   // Look up the name in the global module table.
+  // This code initially does a function name lookup in the LLVM Module's sy-
+  // mbol table. Recall that the LLVM Module is the container that holds the 
+  // functions we are JIT'ing. By giving each function the same name as what 
+  // the user specifies, we can use the LLVM symbol table to resolve function 
+  // names for us.
   Function *CalleeF = TheModule->getFunction(Callee);
   if (!CalleeF)
     return LogErrorV("Unknown function referenced");
@@ -453,6 +506,11 @@ Value *CallExprAST::codegen() {
   if (CalleeF->arg_size() != Args.size())
     return LogErrorV("Incorrect # arguments passed");
 
+  // Once we have the function to call, we recursively codegen each argument 
+  // that is to be passed in, and create an LLVM call instruction. Note that 
+  // LLVM uses the native C calling conventions by default, allowing these 
+  // calls to also call into standard library functions like "sin" and "cos", 
+  // with no additional effort.
   std::vector<Value *> ArgsV;
   for (unsigned i = 0, e = Args.size(); i != e; ++i) {
     ArgsV.push_back(Args[i]->codegen());
@@ -463,16 +521,40 @@ Value *CallExprAST::codegen() {
   return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
 }
 
+// Note first that this function returns a "Function*" instead of a "Value*". 
+// Because a "prototype" really talks about the external interface for a fun-
+// ction (not the value computed by an expression), it makes sense for it to 
+// return the LLVM Function it corresponds to when codegen'd.
 Function *PrototypeAST::codegen() {
   // Make the function type:  double(double,double) etc.
+  // The call to `FunctionType::get` creates the FunctionType that should be 
+  // used for a given Prototype. Since all function arguments in Kaleidoscope 
+  // are of type double, the first line creates a vector of "N" LLVM double 
+  // types. 
   std::vector<Type *> Doubles(Args.size(), Type::getDoubleTy(*TheContext));
+  // It then uses the Functiontype::get method to create a function type that 
+  // takes "N" doubles as arguments, returns one double as a result, and that 
+  // is not vararg (the false parameter indicates this). Note that Types in 
+  // LLVM are uniqued just like Constants are, so you don't "new" a type, you 
+  // "get" it.
   FunctionType *FT =
       FunctionType::get(Type::getDoubleTy(*TheContext), Doubles, false);
-
+  // The line bellow actually creates the IR Function corresponding to the 
+  // Prototype. This indicates the type, linkage and name to use, as well as 
+  // which module to insert into. "external linkage" means that the function 
+  // may be defined outside the current module and/or that it is callable by 
+  // functions outside the module. The Name passed in is the name the user 
+  // specified: since "TheModule" is specified, this name is registered in 
+  // "TheModule"s symbol table.
   Function *F =
       Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
 
   // Set names for all arguments.
+  // Finally, we set the name of each of the function's arguments according 
+  // to the names given in the Prototype. This step isn't strictly necessary, 
+  // but keeping the names consistent makes the IR more readable, and allows 
+  // subsequent code to refer directly to the arguments for their names, ra-
+  // ther than having to look up them up in the Prototype AST.
   unsigned Idx = 0;
   for (auto &Arg : F->args())
     Arg.setName(Args[Idx++]);
@@ -484,21 +566,46 @@ Function *FunctionAST::codegen() {
   // First, check for an existing function from a previous 'extern' declaration.
   Function *TheFunction = TheModule->getFunction(Proto->getName());
 
+  // 首先检查当前模块中是否已经存在一个与给定函数原型（Proto）同名的函数。
+  // For function definitions, we start by searching TheModule's symbol table 
+  // for an existing version of this function, in case one has already been 
+  // created using an 'extern' statement. If Module::getFunction returns null 
+  // then no previous version exists, so we'll codegen one from the Prototype. 
   if (!TheFunction)
+    // 如果不存在，则调用 Proto->codegen() 生成函数原型。
     TheFunction = Proto->codegen();
 
+  // 如果函数原型生成失败，返回 nullptr。
   if (!TheFunction)
     return nullptr;
 
   // Create a new basic block to start insertion into.
+  // Now we get to the point where the Builder is set up. The first line cre-
+  // ates a new basic block (named "entry"), which is inserted into TheFunc-
+  // tion. The second line then tells the builder that new instructions should 
+  // be inserted into the end of the new basic block. Basic blocks in LLVM are 
+  // an important part of functions that define the Control Flow Graph. Since 
+  // we don't have any control flow, our functions will only contain one block 
+  // at this point. We'll fix this in Chapter 5 :).
   BasicBlock *BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
   Builder->SetInsertPoint(BB);
 
   // Record the function arguments in the NamedValues map.
+  // Next we add the function arguments to the NamedValues map (after first 
+  // clearing it out) so that they"re accessible to VariableExprAST nodes.
   NamedValues.clear();
   for (auto &Arg : TheFunction->args())
     NamedValues[std::string(Arg.getName())] = &Arg;
 
+  // Once the insertion point has been set up and the NamedValues map populated, 
+  // we call the codegen() method for the root expression of the function. If no 
+  // error happens, this emits code to compute the expression into the entry block 
+  // and returns the value that was computed. Assuming no error, we then create 
+  // an LLVM ret instruction, which completes the function. Once the function is 
+  // built, we call verifyFunction, which is provided by LLVM. This function does 
+  // a variety of consistency checks on the generated code, to determine if our 
+  // compiler is doing everything right. Using this is important: it can catch a 
+  // lot of bugs. Once the function is finished and validated, we return it.
   if (Value *RetVal = Body->codegen()) {
     // Finish off the function.
     Builder->CreateRet(RetVal);
@@ -510,6 +617,11 @@ Function *FunctionAST::codegen() {
   }
 
   // Error reading body, remove function.
+  // Handling of the error case. For simplicity, we handle this by merely deleting 
+  // the function we produced with the `eraseFromParent` method. This allows the 
+  // user to redefine a function that they incorrectly typed in before: if we did
+  // not delete it, it would live in the symbol table, with a body, preventing fu-
+  // ture redefinition.
   TheFunction->eraseFromParent();
   return nullptr;
 }
